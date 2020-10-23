ceb47fa7b2175ca7d3c15142244d2f3c53acb47f
MetGang <patrick_stritch@wp.pl>
2020-10-23 12:33:12 +0200

    Add "Temperatures" challenge

---
+++ challenges/temperatures.py
@@ -0,0 +1,20 @@
+# https://www.codingame.com/ide/puzzle/temperatures
+
+import math
+
+n = int(input())
+
+if n != 0:
+    closest = 5526
+
+    for i in input().split():
+        t = int(i)
+
+        if abs(t) < abs(closest):
+            closest = t
+        elif abs(t) == abs(closest):
+            closest = abs(t)
+
+    print(closest)
+else:
+    print(0)
